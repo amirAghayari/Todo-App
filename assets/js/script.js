34977fc9112e22951d3a12a0ab146b02a798d01f
amirAghayari <amiraghayari2119@gmail.com>
2024-08-28 13:13:28 +0330

    darkmode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 `use strict`;
+// import darkmode package
 
 // select
 const input = document.querySelector(".todo-input");
@@ -13,9 +14,13 @@ const activeElement = document.querySelectorAll("todo-list");
 const activeTodo = document.querySelector(".active-btn");
 const compledTodo = document.querySelector(".compled-btn");
 const allTodo = document.querySelector(".all-btn");
+const item = document.querySelector(".p-item");
+const darkModeBtn = document.querySelector(".darkmode-layer");
+const exchange = document.querySelector(".exchange-theme");
+console.log(darkModeBtn);
 //  function to create new todo html
 const createTodoHTML = (value) => {
-  return `<div class="todo">
+  return `<li class="todo">
           <div class="div">
             <button class="do-btn"> <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +52,7 @@ const createTodoHTML = (value) => {
               ></path>
             </svg>
           </button>
-        </div>`;
+        </li>`;
 };
 // Func Add Todo
 const addTodoFunc = () => {
@@ -55,6 +60,7 @@ const addTodoFunc = () => {
     if (input.value.trim() !== "") {
       todoList.insertAdjacentHTML("beforeend", createTodoHTML(input.value));
       input.value = "";
+      liNumber();
     }
   });
 
@@ -63,6 +69,7 @@ const addTodoFunc = () => {
       e.preventDefault();
       todoList.insertAdjacentHTML("beforeend", createTodoHTML(input.value));
       input.value = "";
+      liNumber();
     }
   });
 };
@@ -75,6 +82,7 @@ input.addEventListener("click", () => {
 todoList.addEventListener("click", (event) => {
   if (event.target.closest(".delete-btn")) {
     event.target.closest(".todo").remove();
+    decreaseNumber();
   }
   if (event.target.closest(".do-btn")) {
     const todoItem = event.target.closest(".todo").querySelector(".p");
@@ -90,6 +98,7 @@ todoList.addEventListener("click", (event) => {
 // clear completed
 clear.addEventListener("click", () => {
   todoList.innerHTML = "";
+  item.textContent = "0 item";
 });
 
 // active Todo Event
@@ -119,5 +128,73 @@ allTodo.addEventListener("click", () => {
     todo.style.display = "flex";
   });
 });
+
+// number of item
+const liNumber = () => {
+  const number = document.querySelectorAll(".todo").length;
+  item.textContent = `${number} items`;
+};
+
+// reduce number of items
+const decreaseNumber = () => {
+  const number = document.querySelectorAll(".todo").length + 1;
+  item.textContent = `${number - 1} items`;
+};
+
+// dark mode
+const options = {
+  bottom: "64px", // default: '32px'
+  right: "unset", // default: '32px'
+  left: "32px", // default: 'unset'
+  time: "0.3s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "#fff", // default: '#fff'
+  buttonColorDark: "#100f2c", // default: '#100f2c'
+  buttonColorLight: "#fff", // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: "🌓", // default: ''
+  // autoMatchOsTheme: true, // default: true
+};
+
+const darkMode = new Darkmode(options);
+darkMode.showWidget();
+
+////////////////////////
+const sun = `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+            />`;
+const moon = `<svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              />
+            </svg>`;
+
+// exchange icon
+let isSun = true;
+darkModeBtn.addEventListener("click", () => {
+  exchange.innerHTML = isSun ? moon : sun;
+  isSun = !isSun;
+});
+
 // Calling Function
 addTodoFunc();
+sunMoon();
